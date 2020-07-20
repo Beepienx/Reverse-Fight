2d54a9a98f0fcc7ff2631c17fe683a460421904e
Beepinex <beepinex@gmail.com>
2020-07-20 00:25:23 -0700

    player moves well
    no gravity yet

---
+++ Reverse-Fight-Game/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	
+	public CharacterController2D controller;
+	
+	public float RunSpeed = 10f;
+	
+	float HorizontalMove = 0f;
+	
+    void Update()
+    {
+        
+		
+		
+    }
+	
+	void FixedUpdate () 
+	{	
+		HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+		controller.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
+		
+		
+	}
+	
+}
